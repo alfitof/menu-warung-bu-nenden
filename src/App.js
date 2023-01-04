00f437ac82acf74aa63f6830e46c41cf1ec52ffd
alfitof <alfito.fbriansyah@gmail.com>
2023-01-04 15:39:37 +0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,60 +1,52 @@
-import { useState,useEffect } from "react";
-import "./App.css";
-import Card from "./component/Card";
-import data from "./data";
+import { useState, useEffect } from 'react';
+import './App.css';
+import Card from './component/Card';
+import data from './data';
 
 const allCategories = data.map((item) => item.category);
-const uniqueCategories = [...new Set(allCategories)]
+const uniqueCategories = [...new Set(allCategories)];
 
-console.log(uniqueCategories)
+console.log(uniqueCategories);
 
 function App() {
   const [menuFood, setMenuFood] = useState(data);
-  const[categorie,setCategories] =useState([])
+  const [categorie, setCategories] = useState([]);
 
-
-  function toggleType(type){
-    setCategories(type)
-    console.log(type)
+  function toggleType(type) {
+    setCategories(type);
+    console.log(type);
   }
 
-  function filterFood(category){
-    if(category === 'all') {
-      setMenuFood(data)
+  function filterFood(category) {
+    if (category === 'all') {
+      setMenuFood(data);
       return;
     }
-    const newData = data.filter((food) => food.category === category) 
-    setMenuFood(newData)
+    const newData = data.filter((food) => food.category === category);
+    setMenuFood(newData);
   }
-
-
 
   return (
     <div className="App">
-      <h1 className="menu">Our Menu</h1>
+      <h1 className="menu" style={{ marginTop: '4rem' }}>
+        Warung Nasi Bu Nenden
+      </h1>
+      <h2 className="menu">Menu</h2>
       <div className="ligne"></div>
       <ul className="type">
-        <li onClick={()=>filterFood('all')}> All </li>
-        {uniqueCategories.map(c =>
-            <li key={c} onClick={()=>filterFood(c)}> {c} </li>
-          )}
+        <li onClick={() => filterFood('all')}> All </li>
+        {uniqueCategories.map((c) => (
+          <li key={c} onClick={() => filterFood(c)}>
+            {' '}
+            {c}{' '}
+          </li>
+        ))}
       </ul>
 
       <div className="container">
         {menuFood.map((food) => (
-          <Card
-          key={food.id}
-          title={food.title}
-          categorie={food.category}
-          price={food.price}
-          img={food.img}
-          desc={food.desc}/>
-          
-        )
-        )
-}
-          
-        
+          <Card key={food.id} title={food.title} categorie={food.category} price={food.price} img={food.img} desc={food.desc} />
+        ))}
       </div>
     </div>
   );
--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import './Card.css'
-export default function Card({title,categorie,price,img,desc}) {
+import React from 'react';
+import './Card.css';
+export default function Card({ title, categorie, price, img, desc }) {
   return (
     <div className="card">
-        <div className="left">
-            <img src={img} alt='food' className='image'/>
+      <div className="left">
+        <img src={img} alt="food" className="image" />
+      </div>
+      <div className="right">
+        <div className="title">
+          {title} <span className="price"> Rp. {price}</span>
         </div>
-        <div className="right">
-            <div className="title">{title} <span className='price'> $ {price}</span></div>
-            <div className="line"></div>
-            <div className="details">{desc}</div>
-        </div>
-        
-       
+        <div className="line"></div>
+        <div className="details">{desc}</div>
+      </div>
     </div>
-  )
+  );
 }
